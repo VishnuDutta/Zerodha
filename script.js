3cab3a70c380f966e058b609094d956f6f66af2a
Vishnu Dutta <vishnudutta.109@gmail.com>
2024-02-27 02:05:34 +0530

    Done with all the changes it is the Final Result

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ let open = document.getElementById("open")
 function toggleMenu(){
     open.classList.toggle("openmenu");
 }
+
+let subMenu = document.getElementById("subMenu");
+
+function toggleMenu2(){
+    subMenu.classList.toggle("open_menu");
+}
